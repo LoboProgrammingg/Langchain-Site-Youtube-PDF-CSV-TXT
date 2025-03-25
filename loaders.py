75a3b4c38ae11f1618fdc6241d5446e047b54ae7
matheus lobo <klebercamara12@gmail.com>
2025-03-25 12:38:22 -0400

    Tratando erros para caso o site não for acessado

--- loaders.py
+++ loaders.py
@@ -1,10 +1,29 @@
+import os
+
+import streamlit as st
+
+from time import sleep
+
 from langchain_community.document_loaders import WebBaseLoader, YoutubeLoader, CSVLoader, PyPDFLoader, TextLoader
+
+from fake_useragent import UserAgent
 
 
 def carrega_site(url):
-    loader = WebBaseLoader(url)
-    lista_documentos = loader.load()
-    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
+    documento = ''
+    for i in range(5):
+        try:
+            os.environ['USER_AGENT'] = UserAgent().random
+            loader = WebBaseLoader(url, raise_for_status=True)
+            lista_documentos = loader.load()
+            documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
+            break
+        except:
+            print(f'Erro ao carregar o site {i+1}')
+            sleep(3)
+    if documento == '':
+        st.error('Não foi possível carregar o site.')
+        st.stop()
     return documento
 
 
